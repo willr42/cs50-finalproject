1a1a62e78a19b4a6f5fbe1a235bc9feebd96ca26
Will Robertson <williamdrobertson42@gmail.com>
2022-08-01 21:36:07 +1000

    Initial CRA outline

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import RecipeList from './components/RecipeList';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <RecipeList />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/RecipeList.tsx
@@ -0,0 +1,9 @@
+// Main homepage component: Recipe List contains RecipeCards.
+// You can filter the RecipeList by category. Categories appear as pills. Categories are preset.
+
+const RecipeList = () => {
+  return <div className='recipe-list'></div>;
+};
+
+export default RecipeList;
+export {};
