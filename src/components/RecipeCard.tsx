09c42da57fe787e52d5bc7606e4d861c10a4f0db
Will Robertson <williamdrobertson42@gmail.com>
2022-08-04 21:30:58 +1000

    Add RecipeCard component and placeholder styles

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,7 +1,25 @@
 // RecipeCard features an image of the recipe (uploaded by user, stored on deployed server) a title and a category of meal
 // When tapped, it loads a RecipeDetails with the full info.
-const RecipeCard = () => {
-  return <div></div>;
+import styles from './RecipeCard.module.css';
+
+type RecipeCardProps = {
+  name: string;
+  time: string;
+  serves: string;
+};
+
+const RecipeCard: React.FunctionComponent<RecipeCardProps> = ({
+  name,
+  time,
+  serves,
+}) => {
+  return (
+    <div className={styles.recipeCard}>
+      <h1>{name}</h1>
+      <p>{time}</p>
+      <p>{serves}</p>
+    </div>
+  );
 };
 
 export default RecipeCard;
