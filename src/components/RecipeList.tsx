5b91466212d457f46fda2eaaf3e10b3a0263a7d3
Will Robertson <williamdrobertson42@gmail.com>
2022-08-04 21:31:08 +1000

    Add RecipeList component and placeholder styles

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,8 +1,26 @@
 // Main homepage component: Recipe List contains RecipeCards.
 // You can filter the RecipeList by category. Categories appear as pills. Categories are preset.
 
+import recipeJSON from '../temp-recipes.json';
+import RecipeCard from './RecipeCard';
+import styles from './RecipeList.module.css';
+
+const userRecipes = recipeJSON.recipes;
+
 const RecipeList = () => {
-  return <div className='recipe-list'></div>;
+  return (
+    <div className={styles.recipeList}>
+      {userRecipes.map((recipe) => {
+        return (
+          <RecipeCard
+            name={recipe.name}
+            time={recipe.time}
+            serves={recipe.serves}
+          ></RecipeCard>
+        );
+      })}
+    </div>
+  );
 };
 
 export default RecipeList;
