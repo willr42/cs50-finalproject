e520909f4243df99467cdefd5b7396c0e14bcd77
Will Robertson <williamdrobertson42@gmail.com>
2022-08-05 16:00:45 +1000

    Add RecipeDetails initial implementation

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -5,8 +5,42 @@
 // Ingredients Details
 // Method
 // Close
-const RecipeDetails = () => {
-  return <div></div>;
+import { useState } from 'react';
+import styles from './RecipeDetails.module.css';
+import recipeJSON from '../temp-recipes.json';
+
+type RecipeDetailsProps = {
+  id: number;
+};
+
+type Recipe = {
+  id: number;
+  name: string;
+  source: string;
+  time: string;
+  serves: string;
+  ingredients: string[];
+  method: string[];
+};
+
+const userRecipes = recipeJSON.recipes;
+
+const RecipeDetails: React.FunctionComponent<RecipeDetailsProps> = ({ id }) => {
+  // state
+  const [chosenRecipe, setChosenRecipe] = useState<Recipe>();
+  // fetch the detailed recipe
+  let foundRecipe = userRecipes.find((recipe) => {
+    return recipe.id === id;
+  });
+
+  setChosenRecipe(foundRecipe);
+
+  if (chosenRecipe === undefined) {
+    return <></>;
+  }
+
+  // TODO: return a fullscreen modal with a listener to turn off the modal class
+  return <div className={styles.recipeDetails}></div>;
 };
 
 export default RecipeDetails;
