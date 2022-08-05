e39e2f2e3ee5c1f816884c06efe3d2491fee7da3
Will Robertson <williamdrobertson42@gmail.com>
2022-08-05 16:00:27 +1000

    Update RecipeList with RecipeDetails click handler

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,11 +1,13 @@
 // RecipeCard features an image of the recipe (uploaded by user, stored on deployed server) a title and a category of meal
 // When tapped, it loads a RecipeDetails with the full info.
+import { MouseEventHandler } from 'react';
 import styles from './RecipeCard.module.css';
 
 type RecipeCardProps = {
   name: string;
   time: string;
   serves: string;
+  onClick: MouseEventHandler;
 };
 
 const RecipeCard: React.FunctionComponent<RecipeCardProps> = ({
--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,21 +1,32 @@
 // Main homepage component: Recipe List contains RecipeCards.
 // You can filter the RecipeList by category. Categories appear as pills. Categories are preset.
 
+import { useState } from 'react';
 import recipeJSON from '../temp-recipes.json';
 import RecipeCard from './RecipeCard';
+import RecipeDetails from './RecipeDetails';
 import styles from './RecipeList.module.css';
 
 const userRecipes = recipeJSON.recipes;
 
 const RecipeList = () => {
+  const [detailsOpen, setDetailsOpen] = useState(false);
+  const [clickedRecipeID, setClickedRecipeID] = useState(0);
+
   return (
     <div className={styles.recipeList}>
+      {!detailsOpen && <RecipeDetails id={clickedRecipeID}></RecipeDetails>}
+
       {userRecipes.map((recipe) => {
         return (
           <RecipeCard
             name={recipe.name}
             time={recipe.time}
             serves={recipe.serves}
+            onClick={() => {
+              setDetailsOpen(true);
+              setClickedRecipeID(recipe.id);
+            }}
           ></RecipeCard>
         );
       })}
