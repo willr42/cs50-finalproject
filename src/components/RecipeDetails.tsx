00d89ff7f7ee7e8118324fe2ef10eaeab3a3cde8
Will Robertson <williamdrobertson42@gmail.com>
2022-08-08 15:56:01 +1000

    Add RecipeDetails component with styles

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -5,12 +5,14 @@
 // Ingredients Details
 // Method
 // Close
-import { useEffect, useState } from 'react';
+import { MouseEventHandler, useEffect, useState } from 'react';
 import styles from './RecipeDetails.module.css';
 import recipeJSON from '../temp-recipes.json';
+import RecipeTimeAndServe from './RecipeTimeAndServe';
 
 type RecipeDetailsProps = {
   id: number;
+  onClick: MouseEventHandler;
 };
 
 type Recipe = {
@@ -25,7 +27,10 @@ type Recipe = {
 
 const userRecipes = recipeJSON.recipes;
 
-const RecipeDetails: React.FunctionComponent<RecipeDetailsProps> = ({ id }) => {
+const RecipeDetails: React.FunctionComponent<RecipeDetailsProps> = ({
+  id,
+  onClick,
+}) => {
   // state
   const [chosenRecipe, setChosenRecipe] = useState<Recipe>();
 
@@ -45,13 +50,31 @@ const RecipeDetails: React.FunctionComponent<RecipeDetailsProps> = ({ id }) => {
     };
   }, [id]);
 
-  if (!chosenRecipe) {
+  if (!chosenRecipe || id === 0) {
     return <></>;
   }
 
   // return a fullscreen modal with a listener to turn off the modal class
   else {
-    return <div className={styles.recipeDetails}></div>;
+    return (
+      <div className={styles.modalBackground}>
+        <div className={styles.modalContainer}>
+          <div className={styles.recipeContainer}>
+            <h2 className={styles.recipeName}>{chosenRecipe.name}</h2>
+            <h3 className={styles.recipeSource}>{chosenRecipe.source}</h3>
+            <RecipeTimeAndServe
+              time={chosenRecipe.time}
+              serves={chosenRecipe.serves}
+            />
+          </div>
+          <button className={styles.closeModalButton}>
+            <span aria-hidden='true' onClick={onClick}>
+              &times;
+            </span>
+          </button>
+        </div>
+      </div>
+    );
   }
 };
 
