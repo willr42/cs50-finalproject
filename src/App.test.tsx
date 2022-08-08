6029f4ab954a463a2654b0c6407b3a92951029dc
Will Robertson <williamdrobertson42@gmail.com>
2022-08-08 14:37:08 +1000

    Add fonts and default CSS reset

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@fontsource/playfair-display';
+import '@fontsource/lato';
 
 test('renders learn react link', () => {
   render(<App />);
