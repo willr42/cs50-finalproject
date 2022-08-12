764de1d224d2d815cbf74e7f80f12571dafaeb0e
Will Robertson <williamdrobertson42@gmail.com>
2022-08-12 16:05:27 +1000

    Update RecipeDetails with new parsing

--- src/components/RecipeTimeAndServe.tsx
+++ src/components/RecipeTimeAndServe.tsx
@@ -1,9 +1,10 @@
 import styles from './RecipeTimeAndServe.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faUser } from '@fortawesome/free-regular-svg-icons';
+import parseMinutes from '../utils/parseMinutes';
 
 type RecipeTimeProps = {
-  time: string;
+  time: number;
   serves: string;
 };
 
@@ -11,11 +12,13 @@ const RecipeTimeAndServe: React.FunctionComponent<RecipeTimeProps> = ({
   time,
   serves,
 }) => {
+  const displayTime = parseMinutes(time);
+
   return (
     <div className={styles.timeAndServe}>
       <p>
         <FontAwesomeIcon icon={faClock} className={styles.icon} />
-        {time}
+        {displayTime}
       </p>
       <p>
         <FontAwesomeIcon icon={faUser} className={styles.icon} />
