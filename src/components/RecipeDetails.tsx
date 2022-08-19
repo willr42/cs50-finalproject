3df103aeb22ad72052eb71fffe8180c286895b75
Will Robertson <williamdrobertson42@gmail.com>
2022-08-19 14:24:27 +1000

    Refactor RecipeDetails to include Modal

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -5,11 +5,12 @@
 // Ingredients Details
 // Method
 // Close
-import { MouseEventHandler, useEffect, useState } from 'react';
+import { MouseEventHandler, useContext, useEffect, useState } from 'react';
 import styles from './RecipeDetails.module.css';
 import recipeJSON from '../temp-recipes.json';
 import RecipeTimeAndServe from './RecipeTimeAndServe';
 import { RecipeRecord } from './RecipeList';
+import Modal from './Modal';
 
 type RecipeDetailsProps = {
   id: number;
@@ -25,42 +26,22 @@ const RecipeDetails: React.FunctionComponent<RecipeDetailsProps> = ({
   // state
   const [chosenRecipe, setChosenRecipe] = useState<RecipeRecord>();
 
-  // close function. 'any' type to allow us to handle clicks and keyboard presses with the one passed-in function.
-  const closeOnDetail = (event: any) => {
-    onClick(event);
-  };
-
   // TODO: update this to use context passed in. Might not need useEffect - data is already in memory.
   // fetch the detailed recipe
-  useEffect(() => {
-    let ignore = false;
-    let foundRecipe = userRecipes.find((recipe) => {
-      return recipe.id === id;
-    });
-    if (!ignore) {
-      if (foundRecipe) {
-        setChosenRecipe(foundRecipe);
-      }
-    }
-    return () => {
-      ignore = true;
-    };
-  }, [id]);
-
-  useEffect(() => {
-    const keyListener = (event: any) => {
-      console.log(event);
-      if (event.code === 'Escape') {
-        closeOnDetail(event);
-      }
-    };
-
-    document.addEventListener('keydown', keyListener);
-    // cleanup
-    return () => {
-      document.removeEventListener('keydown', keyListener);
-    };
-  });
+  // useEffect(() => {
+  //   let ignore = false;
+  //   let foundRecipe = userRecipes.find((recipe) => {
+  //     return recipe.id === id;
+  //   });
+  //   if (!ignore) {
+  //     if (foundRecipe) {
+  //       setChosenRecipe(foundRecipe);
+  //     }
+  //   }
+  //   return () => {
+  //     ignore = true;
+  //   };
+  // }, [id]);
 
   if (!chosenRecipe || id === 0) {
     return <></>;
@@ -69,37 +50,32 @@ const RecipeDetails: React.FunctionComponent<RecipeDetailsProps> = ({
   // return a fullscreen modal with a listener to turn off the modal class
   else {
     return (
-      <div className={styles.modalBackground}>
-        <div className={styles.modalContainer}>
-          <div className={styles.recipeContainer}>
-            <h2 className={styles.recipeName}>{chosenRecipe.contents.name}</h2>
-            <h3 className={styles.recipeSource}>
-              {chosenRecipe.contents.source}
-            </h3>
-            <RecipeTimeAndServe
-              time={chosenRecipe.contents.time}
-              serves={chosenRecipe.contents.serves}
-            />
-            <div className={styles.ingredientsContainer}>
-              <h3 className={styles.ingredientsTitle}>Ingredients</h3>
-              {chosenRecipe.contents.ingredients.map((ingredient) => (
-                <li className={styles.ingredientsContent}>{ingredient}</li>
-              ))}
-              <p className={styles.ingredientsContent}>{}</p>
-            </div>
-            <div className={styles.methodContainer}>
-              <h3 className={styles.methodTitle}>Method</h3>
-              {chosenRecipe.contents.method.map((step) => (
-                <ol className={styles.methodContent}>{step}</ol>
-              ))}
-              <p className={styles.methodContent}>{}</p>
-            </div>
+      <Modal onClick={onClick}>
+        <div className={styles.recipeContainer}>
+          <h2 className={styles.recipeName}>{chosenRecipe.contents.name}</h2>
+          <h3 className={styles.recipeSource}>
+            {chosenRecipe.contents.source}
+          </h3>
+          <RecipeTimeAndServe
+            time={chosenRecipe.contents.time}
+            serves={chosenRecipe.contents.serves}
+          />
+          <div className={styles.ingredientsContainer}>
+            <h3 className={styles.ingredientsTitle}>Ingredients</h3>
+            {chosenRecipe.contents.ingredients.map((ingredient) => (
+              <li className={styles.ingredientsContent}>{ingredient}</li>
+            ))}
+            <p className={styles.ingredientsContent}>{}</p>
           </div>
-          <button className={styles.closeModalButton} onClick={onClick}>
-            <span aria-hidden='true'>&times;</span>
-          </button>
+          <div className={styles.methodContainer}>
+            <h3 className={styles.methodTitle}>Method</h3>
+            {chosenRecipe.contents.method.map((step) => (
+              <ol className={styles.methodContent}>{step}</ol>
+            ))}
+            <p className={styles.methodContent}>{}</p>
+          </div>
         </div>
-      </div>
+      </Modal>
     );
   }
 };
