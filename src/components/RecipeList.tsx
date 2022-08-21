24e545f8f6b6b9ced41a32af728539fddf2cf54f
Will Robertson <williamdrobertson42@gmail.com>
2022-08-21 19:35:10 +1000

    Refactor RecipeList to use context

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,59 +1,43 @@
 // Main homepage component: Recipe List contains RecipeCards.
 // You can filter the RecipeList by category. Categories appear as pills. Categories are preset.
 
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import RecipeCard from './RecipeCard';
-import RecipeContext from './RecipeContext';
+import { RecipeContext } from './RecipeContext';
 import RecipeDetails from './RecipeDetails';
 import styles from './RecipeList.module.css';
 
-// const userRecipes = recipeJSON.recipes;
-
-type RecipeRecord = {
-  recipe_id: number;
-  contents: {
-    name: string;
-    source: string;
-    time: number;
-    serves: string;
-    ingredients: Array<string>;
-    method: Array<string>;
-  };
-};
-
 const RecipeList = () => {
-  // TODO: update this : make user recipes inside a context so that recipedetails can use that rather than passing it down.
+  const recipes = useContext(RecipeContext);
+
   const [clickedRecipeID, setClickedRecipeID] = useState(0);
-  const [userRecipes, setUserRecipes] = useState<RecipeRecord[]>([]);
 
   return (
-    <RecipeContext>
-      <div>
-        <RecipeDetails
-          id={clickedRecipeID}
-          onClick={() => {
-            setClickedRecipeID(0);
-          }}
-        ></RecipeDetails>
-        <div className={styles.recipeList}>
-          {userRecipes.map((recipe) => {
-            return (
-              <RecipeCard
-                key={recipe.recipe_id}
-                name={recipe.contents.name}
-                time={recipe.contents.time}
-                serves={recipe.contents.serves}
-                onClick={() => {
-                  setClickedRecipeID(recipe.recipe_id);
-                }}
-              ></RecipeCard>
-            );
-          })}
-        </div>
+    <div>
+      <RecipeDetails
+        recipes={recipes}
+        id={clickedRecipeID}
+        onClick={() => {
+          setClickedRecipeID(0);
+        }}
+      ></RecipeDetails>
+      <div className={styles.recipeList}>
+        {recipes.map((recipe) => {
+          return (
+            <RecipeCard
+              key={recipe.recipe_id}
+              name={recipe.contents.name}
+              time={recipe.contents.time}
+              serves={recipe.contents.serves}
+              onClick={() => {
+                setClickedRecipeID(recipe.recipe_id);
+              }}
+            ></RecipeCard>
+          );
+        })}
       </div>
-    </RecipeContext>
+    </div>
   );
 };
 
 export default RecipeList;
-export type { RecipeRecord };
