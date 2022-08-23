9a420746b86f57561cb5eae5fd94d8003cac7e81
Will Robertson <williamdrobertson42@gmail.com>
2022-08-23 11:57:51 +1000

    Add new Home component to conditionally render

---
+++ src/components/Home.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, FC, useContext } from 'react'; //types
+import Login from './Login';
+import { RecipeContextProvider } from './RecipeContext';
+import RecipeList from './RecipeList';
+import { UserContext } from './UserContext';
+
+type Props = {
+  children?: ReactNode;
+};
+
+const Home: FC<Props> = ({ children }) => {
+  const ctx = useContext(UserContext);
+  if (ctx.isLoggedIn) {
+    return (
+      <RecipeContextProvider>
+        <RecipeList />
+      </RecipeContextProvider>
+    );
+  } else {
+    return (
+      <div>
+        <Login />
+      </div>
+    );
+  }
+};
+
+export default Home;
