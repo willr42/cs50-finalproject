25d2038396239823b7eca9bd5bf39dbfc2355903
Will Robertson <williamdrobertson42@gmail.com>
2022-09-06 16:22:46 +1000

    Move into subfolder, add change event handler

--- src/components/recipeView/recipeAdd/RecipeAdd.tsx
+++ src/components/recipeView/recipeAdd/RecipeAdd.tsx
@@ -1,8 +1,9 @@
-import Modal from '../Modal';
-import { useContext, useState } from 'react';
-import { RecipeContext } from '../RecipeContext';
+import React, { useContext, useState } from 'react';
+import { RecipeContext } from '../../RecipeContext';
+import Modal from '../../Modal';
 import styles from './RecipeAdd.module.css';
-import { RecipeRecord } from '../types';
+import { RecipeContents } from '../../types';
+import recipeFormStateUpdate from './recipeFormStateUpdate';
 // will need to call a utility function to add a new recipe to DB
 
 // form needs to reflect RecipeRecord type
@@ -10,7 +11,7 @@ import { RecipeRecord } from '../types';
 const RecipeAdd = () => {
   const recipes = useContext(RecipeContext);
   const [addShowing, setAddShowing] = useState(false);
-  const [recipeContents, setRecipeContents] = useState('');
+  const [recipeContents, setRecipeContents] = useState<RecipeContents>();
   return (
     <>
       {addShowing ? (
@@ -18,7 +19,13 @@ const RecipeAdd = () => {
           <form className='form' autoComplete='false'>
             <label>
               <p className={styles.paragraphLabel}>Recipe Title</p>
-              <input className='textInput' type='text' />
+              <input
+                className='textInput'
+                type='text'
+                onChange={(e) =>
+                  recipeFormStateUpdate(e, 'name', setRecipeContents)
+                }
+              />
             </label>
             <label>
               <p className={styles.paragraphLabel}>Source</p>
@@ -26,6 +33,9 @@ const RecipeAdd = () => {
                 className='textInput'
                 type='text'
                 placeholder='Where did you find it?'
+                onChange={(e) =>
+                  recipeFormStateUpdate(e, 'source', setRecipeContents)
+                }
               />
             </label>
             <label>
@@ -34,6 +44,9 @@ const RecipeAdd = () => {
                 className='textInput'
                 type='text'
                 placeholder='eg. 01:30'
+                onChange={(e) =>
+                  recipeFormStateUpdate(e, 'time', setRecipeContents)
+                }
               />
             </label>
             <label>
@@ -43,6 +56,9 @@ const RecipeAdd = () => {
                 type='number'
                 placeholder='Serves how many?'
                 min='1'
+                onChange={(e) =>
+                  recipeFormStateUpdate(e, 'serves', setRecipeContents)
+                }
               />
             </label>
             <label>
@@ -51,6 +67,9 @@ const RecipeAdd = () => {
                 className='textAreaInput'
                 placeholder='100g mushrooms&#10;100ml milk'
                 rows={10}
+                onChange={(e) =>
+                  recipeFormStateUpdate(e, 'ingredients', setRecipeContents)
+                }
               />
             </label>
             <label>
@@ -59,6 +78,9 @@ const RecipeAdd = () => {
                 className='textAreaInput'
                 placeholder='1. Preheat oven to 180°C.&#10;2.Chop up the mushrooms.'
                 rows={10}
+                onChange={(e) =>
+                  recipeFormStateUpdate(e, 'method', setRecipeContents)
+                }
               />
             </label>
           </form>
