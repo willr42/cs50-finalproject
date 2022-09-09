12f03452c89583e685b9c9729b6f31fc1480bd43
Will Robertson <williamdrobertson42@gmail.com>
2022-09-09 13:50:53 +1000

    Break out text-area handling in recipe submit

--- src/components/recipeView/recipeAdd/recipeFormStateUpdate.ts
+++ src/components/recipeView/recipeAdd/recipeFormStateUpdate.ts
@@ -1,13 +1,34 @@
+import React from 'react';
 import { RecipeContents } from '../../types';
 
-export default function recipeFormStateUpdate(
-  event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+/**
+ *
+ * @param event React ChangeEvent
+ * @param value value in recipe to be updated
+ * @param stateUpdater state update function
+ */
+export function recipeFormInputUpdate(
+  event: React.ChangeEvent<HTMLInputElement>,
   value: string | number,
   stateUpdater: Function
 ) {
   stateUpdater((previousState: RecipeContents) => {
     const newValue: Partial<RecipeContents> = {
       [value]: event.target.value,
+    };
+    return Object.assign({}, previousState, newValue);
+  });
+}
+
+export function recipeFormTextAreaUpdate(
+  event: React.ChangeEvent<HTMLTextAreaElement>,
+  value: string | number,
+  stateUpdater: Function
+) {
+  const valueArray = event.target.value.split('\n');
+  stateUpdater((previousState: RecipeContents) => {
+    const newValue: Partial<RecipeContents> = {
+      [value]: valueArray,
     };
     return Object.assign({}, previousState, newValue);
   });
